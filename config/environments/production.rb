a1e338f73439a7c9b47ee8b543028c424c41159f
Bergson Lima <bergsonsud@gmail.com>
2019-12-17 02:21:59 -0300

    Tentando4 corrigir erro1 - assets adicionados inicializador

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,13 +33,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
-
-  config.assets.debug = true
-
-  # Suppress logger output for asset requests.
-  config.assets.quiet = true
-  
+  config.assets.compile = false
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
