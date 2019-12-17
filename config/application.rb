0a9b0a2b2e3fbc8ec08550925e3b7d063d8b6c41
Bergson Lima <bergsonsud@gmail.com>
2019-12-17 03:19:24 -0300

    Tentando4 corrigir erro1 - assets adicionados inicializador 3

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,18 @@ module Iptv
     config.load_defaults 5.2
     config.serve_static_assets = true
 
+
+    #config.load_defaults 5.2
+    config.assets.paths << Rails.root.join("iptv", "assets", "fonts")
+    config.assets.paths << Rails.root.join("iptv", "assets", "images")
+    config.assets.paths << Rails.root.join("iptv", "assets", "bootstrap")
+    config.assets.paths << Rails.root.join("iptv", "assets", "rs-plugin-5")
+    config.assets.paths << Rails.root.join("iptv", "assets", "magnific-popup")
+    config.assets.paths << Rails.root.join("iptv", "assets", "owlcarousel2")
+    config.assets.paths << Rails.root.join("iptv", "assets", "morphext")
+
+    config.assets.paths << Rails.root.join('vendor', 'assets', 'components', 'gentelella', 'production')
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
