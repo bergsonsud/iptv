d01202c06cb451ef39873ec9ed8a406665eda452
Bergson Lima <bergsonsud@gmail.com>
2019-12-19 00:49:31 -0300

    Ajustado funcionando em producao local

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,8 +12,9 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
-
+Rails.application.config.assets.paths << Rails.root.join("app", "assets", "fonts")
 Rails.application.config.assets.precompile += %w( animate.min.css )
+Rails.application.config.assets.precompile += %w( main.css )
 Rails.application.config.assets.precompile += %w( bootstrap.min.css )
 Rails.application.config.assets.precompile += %w( font-awesome.min.css )
 Rails.application.config.assets.precompile += %w( hamburgers.min.css )
