5dde605231af33ef95fea6b10f73eb4efaf2ac1b
Bergson Lima <bergsonsud@gmail.com>
2019-12-19 02:01:35 -0300

    Fix

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module Iptv
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.serve_static_assets = true
-
+    #config.assets.precompile += %w( teste.css )
 
     #config.load_defaults 5.2
     # config.assets.paths << Rails.root.join("app", "assets", "fonts")
