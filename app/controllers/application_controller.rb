f2fb0d7a5913f6478778b8b4bc819e045c3d0811
Bergson Lima <bergsonsud@gmail.com>
2019-12-21 01:54:50 -0300

    Fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+before_action :expire_hsts
+
+
+private
+  def expire_hsts
+    response.headers["Strict-Transport-Security"] = 'max-age=0'
+  end	
 end
